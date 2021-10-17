15c1a96b88c6f49dae04889eddfc15714915270b
Jack Baldry <jack.baldry@grafana.com>
2021-10-17 18:04:23 +0100

    doc: Clarify that the editor Elisp registers the server with lsp-mode
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- editor/jsonnet-language-server.el
+++ editor/jsonnet-language-server.el
@@ -2,6 +2,7 @@
 ;; Development lsp registration for Emacs lsp-mode.
 ;;; Commentary:
 ;;; Code:
+(require 'jsonnet-mode)
 (require 'lsp-mode)
 
 (defcustom lsp-jsonnet-executable "jsonnet-language-server"
@@ -9,12 +10,20 @@
   :group 'lsp-jsonnet
   :risky t
   :type 'file)
+
+;; Configure lsp-mode language identifiers.
+;; If you use jsonnet-mode, you only need the first configuration.
+;; If not, you probably need both the file regexps.
 (add-to-list 'lsp-language-id-configuration '(jsonnet-mode . "jsonnet"))
+
+;; Register jsonnet-language-server with the LSP client.
 (lsp-register-client
  (make-lsp-client
   :new-connection (lsp-stdio-connection (lambda () lsp-jsonnet-executable))
   :activation-fn (lsp-activate-on "jsonnet")
   :server-id 'jsonnet))
+
+;; Start the language server whenever jsonnet-mode is used.
 (add-hook 'jsonnet-mode-hook #'lsp-deferred)
 
 (provide 'jsonnet-language-server)
