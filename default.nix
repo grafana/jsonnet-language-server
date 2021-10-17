ce55956568c194821b21644ecd783ec645b546a8
Jack Baldry <jack.baldry@grafana.com>
2021-10-17 22:17:04 +0100

    chore: License project under AGPLv3
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- default.nix
+++ default.nix
@@ -8,5 +8,10 @@ buildGoModule rec {
   src = lib.cleanSource ./.;
   vendorSha256 = "sha256-jMA+lEzQ60p5JsFdPisIy0fK0f1N0RG9yfqpq4uOIn0=";
 
-  meta = with lib; { maintainers = with maintainers; [ jdbaldry ]; };
+  meta = with lib; {
+    description = "A Language Server Protocol server for Jsonnet";
+    homepage = "https://github.com/jdbaldry/jsonnet-language-server";
+    license = licenses.agpl3;
+    maintainers = with maintainers; [ jdbaldry ];
+  };
 }
