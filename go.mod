d5938820fcf24d38421c1ac4765becec55ed47ce
Jack Baldry <jack.baldry@grafana.com>
2021-10-17 17:16:51 +0100

    chore: Use internal gopls language server internals
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jdbaldry/jsonnet-language-server
 
 go 1.16
+
+require github.com/jdbaldry/go-language-server-protocol v0.0.0-20211013214444-3022da0884b2
--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"github.com/jdbaldry/go-language-server-protocol/jsonrpc2"
+)
+
+type stdio struct{}
+
+// Read implements io.Reader interface.
+func (stdio) Read(b []byte) (int, error) { return os.Stdin.Read(b) }
+
+// Write implements io.Writer interface.
+func (stdio) Write(b []byte) (int, error) { return os.Stdout.Write(b) }
+
+// Close implements io.Closer interface.
+func (stdio) Close() error {
+	if err := os.Stdin.Close(); err != nil {
+		return err
+	}
+	return os.Stdout.Close()
+}
+
+// LocalAddr implements net.Conn interface.
+func (s stdio) LocalAddr() net.Addr { return s }
+
+// RemoteAddr implements net.Conn interface.
+func (s stdio) RemoteAddr() net.Addr { return s }
+
+// SetDeadline implements net.Conn interface.
+func (stdio) SetDeadline(t time.Time) error { return nil }
+
+// SetReadDeadline implements net.Conn interface.
+func (stdio) SetReadDeadline(t time.Time) error { return nil }
+
+// SetWriteDeadline implements net.Conn interface.
+func (stdio) SetWriteDeadline(t time.Time) error { return nil }
+
+// Network implements net.Addr interface.
+func (stdio) Network() string { return "stdio" }
+
+// String implements net.Addr interface.
+func (stdio) String() string { return "stdio" }
 
 func main() {
-	fmt.Println("Hello world!")
+	ctx := context.TODO()
+	stream := jsonrpc2.NewHeaderStream(stdio{})
+	conn := jsonrpc2.NewConn(stream)
+	conn.Go(ctx, nil)
+	<-conn.Done()
+	if err := conn.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
 }
