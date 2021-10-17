761286cdc229cd1cf8c144ad7bbc97a76c9b59a0
Jack Baldry <jack.baldry@grafana.com>
2021-10-17 17:16:52 +0100

    chore: Move editor integration into subdirectory
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- editor/jsonnet-language-server.el
+++ editor/jsonnet-language-server.el
@@ -1,20 +1,20 @@
-;;; lsp -- Summary
+;;; jsonnet-language-server -- Summary
 ;; Development lsp registration for Emacs lsp-mode.
 ;;; Commentary:
 ;;; Code:
 (require 'lsp-mode)
 
 (defcustom lsp-jsonnet-executable "jsonnet-language-server"
-  (add-to-list 'lsp-language-id-configuration '(jsonnet-mode . "jsonnet"))
   "Command to start the Jsonnet language server."
   :group 'lsp-jsonnet
   :risky t
   :type 'file)
+(add-to-list 'lsp-language-id-configuration '(jsonnet-mode . "jsonnet"))
 (lsp-register-client
  (make-lsp-client
   :new-connection (lsp-stdio-connection (lambda () lsp-jsonnet-executable))
   :activation-fn (lsp-activate-on "jsonnet")
   :server-id 'jsonnet))
 
-(provide 'lsp)
-;;; lsp.el ends here
+(provide 'jsonnet-language-server)
+;;; jsonnet-language-server.el ends here
