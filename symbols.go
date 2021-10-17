84e0f6fabe0be10138a209694f116896bbd25d54
Jack Baldry <jack.baldry@grafana.com>
2021-10-17 21:56:54 +0100

    Add TODO(#7)

--- symbols.go
+++ symbols.go
@@ -201,7 +201,7 @@ func analyseSymbols(n ast.Node) (symbols []protocol.DocumentSymbol) {
 			// Adding five (five minus the one for zero indexing plus one for a space) to the location range of the local
 			// symbol gets closer to the real location but any amount of whitespace could be inbetween.
 			// Assuming a single space, this works perfectly.
-			// TODO: Understand why identifiers in local function binds are missing.
+			// TODO(#7): Understand why identifiers in local function binds are missing.
 			if bind.LocRange.Begin.Line == 0 {
 				binds[i].Range = protocol.Range{
 					Start: protocol.Position{Line: uint32(n.Loc().Begin.Line - 1), Character: uint32(n.Loc().Begin.Column + 5)},
