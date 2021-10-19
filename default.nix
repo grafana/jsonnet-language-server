81b98b6afdb2c5b87374d80b0b9e971d53202890
Jack Baldry <jack.baldry@grafana.com>
2021-10-19 14:41:04 +0100

    feat: Announce v0.1.2 release
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 with pkgs;
 buildGoModule rec {
   pname = "jsonnet-language-server";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = lib.cleanSource ./.;
   vendorSha256 = "sha256-jMA+lEzQ60p5JsFdPisIy0fK0f1N0RG9yfqpq4uOIn0=";
