1ccce66ba851dd006e5993ad76730f43b1dcac0d
Jack Baldry <jack.baldry@grafana.com>
2021-11-22 05:45:05 +0000

    Format Nix files with nixpkgs-fmt
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- flake.nix
+++ flake.nix
@@ -7,20 +7,25 @@
   outputs = { self, nixpkgs, flake-utils }:
     {
       overlay =
-        (final: prev: {
-          jsonnet-language-server = prev.callPackage ./default.nix { pkgs = prev; };
-          snitch = prev.callPackage ./snitch.nix { pkgs = prev; };
-        });
-    } //
-    (flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
-      in
-      {
-        defaultPackage = pkgs.jsonnet-language-server;
-        devShell = import ./shell.nix { inherit pkgs; };
-        packages = {
-          jsonnet-tool = pkgs.jsonnet-language-server;
-        };
-      }
-    ));
+        (
+          final: prev: {
+            jsonnet-language-server = prev.callPackage ./default.nix { pkgs = prev; };
+            snitch = prev.callPackage ./snitch.nix { pkgs = prev; };
+          }
+        );
+    } // (
+      flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
+        in
+        {
+          defaultPackage = pkgs.jsonnet-language-server;
+          devShell = import ./shell.nix { inherit pkgs; };
+          packages = {
+            jsonnet-tool = pkgs.jsonnet-language-server;
+          };
+        }
+      )
+    );
 }
