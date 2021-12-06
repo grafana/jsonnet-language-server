6d2d9010d1555033f580a487c63d067ed9be6cfc
Julien Duchesne <julien.duchesne@grafana.com>
2021-12-06 14:17:46 -0500

    Basic hover support for std

--- server/hover.go
+++ server/hover.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/google/go-jsonnet/ast"
 	"github.com/jdbaldry/go-language-server-protocol/lsp/protocol"
 )
 
@@ -23,7 +25,7 @@ func (s *server) Hover(ctx context.Context, params *protocol.HoverParams) (*prot
 
 	_, node := stack.Pop()
 
-	// DEBUG
+	// // DEBUG
 	// r := protocol.Range{
 	// 	Start: protocol.Position{
 	// 		Line:      uint32(node.Loc().Begin.Line) - 1,
@@ -34,24 +36,36 @@ func (s *server) Hover(ctx context.Context, params *protocol.HoverParams) (*prot
 	// 		Character: uint32(node.Loc().End.Column) - 1,
 	// 	},
 	// }
-	// return &protocol.Hover{Range: r, Contents: protocol.MarkupContent{Kind: protocol.PlainText, Value: fmt.Sprintf("%v: %+v", reflect.TypeOf(node), node)}}, nil
+	// return &protocol.Hover{Range: r,
+	// 	Contents: protocol.MarkupContent{Kind: protocol.PlainText,
+	// 		Value: fmt.Sprintf("%v: %+v\n\n%v: %+v", reflect.TypeOf(node), node, reflect.TypeOf(node2), node2)},
+	// }, nil
 
-	if vars := node.FreeVariables(); len(vars) > 0 && vars[0] == "std" {
-		lineIndex := uint32(node.Loc().Begin.Line) - 1
-		startIndex := uint32(node.Loc().Begin.Column) - 1
-		// line := strings.Split(doc.item.Text, "\n")[lineIndex]
+	_, isIndex := node.(*ast.Index)
+	_, isVar := node.(*ast.Var)
+	lineIndex := uint32(node.Loc().Begin.Line) - 1
+	startIndex := uint32(node.Loc().Begin.Column) - 1
+	line := strings.Split(doc.item.Text, "\n")[lineIndex]
+	if isIndex || isVar && strings.HasPrefix(line[startIndex:], "std") {
+		functionNameIndex := startIndex + 4
+		if functionNameIndex < uint32(len(line)) {
+			functionName := strings.Split(line[functionNameIndex:], "(")[0]
+			functionName = strings.TrimSpace(functionName)
 
-		r := protocol.Range{
-			Start: protocol.Position{
-				Line:      lineIndex,
-				Character: startIndex,
-			},
-			End: protocol.Position{
-				Line:      uint32(node.Loc().End.Line) - 1,
-				Character: uint32(node.Loc().End.Column) - 1,
-			},
+			for _, function := range s.stdlib {
+				if function.Name == functionName {
+					return &protocol.Hover{
+						Range: protocol.Range{
+							Start: protocol.Position{Line: lineIndex, Character: startIndex},
+							End:   protocol.Position{Line: lineIndex, Character: functionNameIndex + uint32(len(functionName))}},
+						Contents: protocol.MarkupContent{
+							Kind:  protocol.Markdown,
+							Value: fmt.Sprintf("`std.%s(%s)`\n\n%s", function.Name, strings.Join(function.Params, ", "), function.MarkdownDescription),
+						},
+					}, nil
+				}
+			}
 		}
-		return &protocol.Hover{Range: r, Contents: protocol.MarkupContent{Kind: protocol.PlainText, Value: fmt.Sprintf("test")}}, nil
 	}
 
 	return nil, nil
