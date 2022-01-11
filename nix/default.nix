5e0063f71bd01ae4bb53c22fe7696641c184be6d
Julien Duchesne <julien.duchesne@grafana.com>
2022-01-11 07:25:33 -0500

    Release 0.6.4

--- nix/default.nix
+++ nix/default.nix
@@ -3,13 +3,13 @@
 with pkgs;
 buildGoModule rec {
   pname = "jsonnet-language-server";
-  version = "0.6.3";
+  version = "0.6.4";
 
   ldflags = ''
     -X main.version=${version}
   '';
   src = lib.cleanSource ../.;
-  vendorSha256 = "sha256-mGocX5z3wf9KRhE9leLNCzn8sVdjKJo6FzgP1OwQB3M=";
+  vendorSha256 = "sha256-DvAJwWfDqMxMNicerHa7fUGuT3/BEOAJ3PhKnQcIvFg=";
 
   meta = with lib; {
     description = "A Language Server Protocol server for Jsonnet";
