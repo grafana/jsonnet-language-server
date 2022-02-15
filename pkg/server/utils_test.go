1233a002566e643de85aaf54a808f73ec320227d
Julien Duchesne <julienduchesne@live.com>
2022-02-15 17:54:13 -0500

    Go-to-definition: Return a list of Locations all the way to the top (#28)
    The `processing.FindRangesFromIndexList` function already returned a list of object ranges (never more than one in it for now, though)
    However, this list didn't make it up to the server's `Definition` method.
    
    This refactors the server code and the tests to allow us to return and test for a list of locations. All that will remain (so easy, right) is to modify the `processing` package code
    
    Progress towards https://github.com/grafana/jsonnet-language-server/issues/6

--- pkg/server/utils_test.go
+++ pkg/server/utils_test.go
@@ -3,14 +3,24 @@ package server
 import (
 	"context"
 	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/google/go-jsonnet"
 	"github.com/grafana/jsonnet-language-server/pkg/stdlib"
 	"github.com/grafana/jsonnet-language-server/pkg/utils"
 	"github.com/jdbaldry/go-language-server-protocol/jsonrpc2"
 	"github.com/jdbaldry/go-language-server-protocol/lsp/protocol"
 	"github.com/stretchr/testify/require"
 )
+
+func absUri(t *testing.T, path string) protocol.DocumentURI {
+	t.Helper()
+
+	abs, err := filepath.Abs(path)
+	require.NoError(t, err)
+	return protocol.URIFromPath(abs)
+}
 
 func testServer(t *testing.T, stdlib []stdlib.Function) (server *server) {
 	t.Helper()
@@ -26,6 +36,24 @@ func testServer(t *testing.T, stdlib []stdlib.Function) (server *server) {
 	return server
 }
 
+func serverOpenTestFile(t *testing.T, server *server, filename string) protocol.DocumentURI {
+	fileContent, err := os.ReadFile(filename)
+	require.NoError(t, err)
+
+	uri := protocol.URIFromPath(filename)
+	err = server.DidOpen(context.Background(), &protocol.DidOpenTextDocumentParams{
+		TextDocument: protocol.TextDocumentItem{
+			URI:        uri,
+			Text:       string(fileContent),
+			Version:    1,
+			LanguageID: "jsonnet",
+		},
+	})
+	require.NoError(t, err)
+
+	return uri
+}
+
 func testServerWithFile(t *testing.T, stdlib []stdlib.Function, fileContent string) (server *server, fileURI protocol.DocumentURI) {
 	t.Helper()
 
@@ -33,20 +61,15 @@ func testServerWithFile(t *testing.T, stdlib []stdlib.Function, fileContent stri
 
 	tmpFile, err := os.CreateTemp("", "")
 	require.NoError(t, err)
-	uri := protocol.URIFromPath(tmpFile.Name())
 
 	_, err = tmpFile.WriteString(fileContent)
 	require.NoError(t, err)
 
-	err = server.DidOpen(context.Background(), &protocol.DidOpenTextDocumentParams{
-		TextDocument: protocol.TextDocumentItem{
-			URI:        uri,
-			Text:       fileContent,
-			Version:    1,
-			LanguageID: "jsonnet",
-		},
-	})
-	require.NoError(t, err)
+	return server, serverOpenTestFile(t, server, tmpFile.Name())
+}
 
-	return server, uri
+func testGetVM(path string) (*jsonnet.VM, error) {
+	vm := jsonnet.MakeVM()
+	vm.Importer(&jsonnet.FileImporter{JPaths: []string{"testdata"}})
+	return vm, nil
 }
