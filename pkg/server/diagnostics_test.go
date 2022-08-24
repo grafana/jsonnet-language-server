fe63f3d87e8f31f3ed1ca1ee90c79adf4d79fa45
Julien Duchesne <julienduchesne@live.com>
2022-08-24 07:18:33 -0400

    Fix diagnostics line parsing (#50)
    Fixes https://github.com/grafana/jsonnet-language-server/issues/49
    The parsing was all wrong in some cases and was quite unsafe. This should no longer panic and it fixes a few issues with parsing
    All tested now, too

--- pkg/server/diagnostics_test.go
+++ pkg/server/diagnostics_test.go
@@ -14,6 +14,28 @@ func TestGetLintDiags(t *testing.T) {
 		expected    []protocol.Diagnostic
 	}{
 		{
+			name:        "no error",
+			fileContent: `{}`,
+		},
+		{
+			name: "invalid function call",
+			fileContent: `function(notPassed) {
+	this: 'is wrong',
+}()
+`,
+			expected: []protocol.Diagnostic{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 0, Character: 20},
+						End:   protocol.Position{Line: 2, Character: 3},
+					},
+					Severity: protocol.SeverityWarning,
+					Source:   "lint",
+					Message:  "Called value must be a function, but it is assumed to be an object",
+				},
+			},
+		},
+		{
 			name: "unused variable",
 			fileContent: `
 local unused = 'test';
@@ -41,6 +63,94 @@ local unused = 'test';
 			}
 
 			diags := s.getLintDiags(doc)
+			assert.Equal(t, tc.expected, diags)
+		})
+	}
+}
+
+func TestGetEvalDiags(t *testing.T) {
+	testCases := []struct {
+		name        string
+		fileContent string
+		expected    []protocol.Diagnostic
+	}{
+		{
+			name:        "no error",
+			fileContent: `{}`,
+		},
+		{
+			name:        "syntax error 1",
+			fileContent: `{ s }`,
+			expected: []protocol.Diagnostic{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 0, Character: 4},
+						End:   protocol.Position{Line: 0, Character: 5},
+					},
+					Severity: protocol.SeverityError,
+					Source:   "jsonnet evaluation",
+					Message:  `Expected token OPERATOR but got "}"`,
+				},
+			},
+		},
+		{
+			name:        "syntax error 2",
+			fileContent: `{ s: }`,
+			expected: []protocol.Diagnostic{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 0, Character: 5},
+						End:   protocol.Position{Line: 0, Character: 6},
+					},
+					Severity: protocol.SeverityError,
+					Source:   "jsonnet evaluation",
+					Message:  `Unexpected: "}" while parsing terminal`,
+				},
+			},
+		},
+		{
+			name:        "syntax error 3",
+			fileContent: `{`,
+			expected: []protocol.Diagnostic{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 0, Character: 1},
+						End:   protocol.Position{Line: 0, Character: 1},
+					},
+					Severity: protocol.SeverityError,
+					Source:   "jsonnet evaluation",
+					Message:  `Unexpected: end of file while parsing field definition`,
+				},
+			},
+		},
+		{
+			name: "syntax error 4",
+			fileContent: `{ 
+    s: |||
+|||
+}`,
+			expected: []protocol.Diagnostic{
+				{
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 1, Character: 7},
+						End:   protocol.Position{Line: 1, Character: 7},
+					},
+					Severity: protocol.SeverityError,
+					Source:   "jsonnet evaluation",
+					Message:  `Text block's first line must start with whitespace`,
+				},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			s, fileURI := testServerWithFile(t, nil, tc.fileContent)
+			doc, err := s.cache.get(fileURI)
+			if err != nil {
+				t.Fatalf("%s: %v", errorRetrievingDocument, err)
+			}
+
+			diags := s.getEvalDiags(doc)
 			assert.Equal(t, tc.expected, diags)
 		})
 	}
