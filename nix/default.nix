0f265f78b23535f0a148e2ecaa23c6490a896d7a
Trevor Whitney <trevorjwhitney@gmail.com>
2024-07-05 10:03:48 -0600

    fix: update nix licenses

--- nix/default.nix
+++ nix/default.nix
@@ -14,7 +14,7 @@ buildGoModule rec {
   meta = with lib; {
     description = "A Language Server Protocol server for Jsonnet";
     homepage = "https://github.com/grafana/jsonnet-language-server";
-    license = licenses.agpl3;
+    license = licenses.agpl3Only;
     maintainers = with maintainers; [ jdbaldry trevorwhitney ];
   };
 }
