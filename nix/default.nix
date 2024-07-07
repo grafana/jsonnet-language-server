f055eadd097f4176d99d85136061c81d13ca7358
Trevor Whitney <trevorjwhitney@gmail.com>
2024-07-07 17:34:15 -0600

    fix: update nix package (#145)

--- nix/default.nix
+++ nix/default.nix
@@ -5,16 +5,16 @@ buildGoModule rec {
   pname = "jsonnet-language-server";
   version = "0.13.1";
 
-  ldflags = ''
-    -X main.version=${version}
-  '';
+  ldflags = [
+    "-X main.version=${version}"
+  ];
   src = lib.cleanSource ../.;
   vendorHash = "sha256-+9Eh40kkyZc9mS4m6BqK5PweFRUA0iWgsG/h2jZJr5w=";
 
   meta = with lib; {
     description = "A Language Server Protocol server for Jsonnet";
     homepage = "https://github.com/grafana/jsonnet-language-server";
-    license = licenses.agpl3;
+    license = licenses.agpl3Only;
     maintainers = with maintainers; [ jdbaldry trevorwhitney ];
   };
 }
