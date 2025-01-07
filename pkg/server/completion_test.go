2412c8b058637f701afce47b08562b4607523468
Dale A. Jackson <3943722+JacksonWrath@users.noreply.github.com>
2025-01-07 05:50:15 -0800

    Completion within parentheses (#179)
    * Enables completion when typing inside parentheses
      (e.g. in-line arguments of a function call)
    * Also fixes some edge-case bugs causing the server to crash
      (e.g. initiating auto-complete on an empty line)

--- pkg/server/completion_test.go
+++ pkg/server/completion_test.go
@@ -692,6 +692,53 @@ func TestCompletion(t *testing.T) {
 				},
 			},
 		},
+		{
+			name:            "completion in function arguments",
+			filename:        "testdata/functions.libsonnet",
+			replaceString:   "test: myfunc(arg1, arg2)",
+			replaceByString: "test: myfunc(arg1, self.",
+			expected: protocol.CompletionList{
+				IsIncomplete: false,
+				Items: []protocol.CompletionItem{
+					{
+						Label:      "a",
+						Kind:       protocol.FieldCompletion,
+						Detail:     "self.a",
+						InsertText: "a",
+						LabelDetails: protocol.CompletionItemLabelDetails{
+							Description: "variable",
+						},
+					},
+					{
+						Label:      "b",
+						Kind:       protocol.FieldCompletion,
+						Detail:     "self.b",
+						InsertText: "b",
+						LabelDetails: protocol.CompletionItemLabelDetails{
+							Description: "variable",
+						},
+					},
+					{
+						Label:      "c",
+						Kind:       protocol.FieldCompletion,
+						Detail:     "self.c",
+						InsertText: "c",
+						LabelDetails: protocol.CompletionItemLabelDetails{
+							Description: "string",
+						},
+					},
+					{
+						Label:      "test",
+						Kind:       protocol.FieldCompletion,
+						Detail:     "self.test",
+						InsertText: "test",
+						LabelDetails: protocol.CompletionItemLabelDetails{
+							Description: "*ast.Apply",
+						},
+					},
+				},
+			},
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
